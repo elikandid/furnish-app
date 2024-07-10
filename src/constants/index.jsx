35a0429597e8b13e0853d1df32f7e2f24ce80281
unknown <edogbey64@gmail.com>
2024-07-10 12:32:33 +0000

    Upd

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,3 +1,5 @@
+import { HeartHandshake } from "lucide-react";
+
 const K= {
     NAVLINKS: [
         {
@@ -25,14 +27,15 @@ const K= {
         {
             title: "Amazing Deals",
             description: "Far far away, behind the word mountains, far from the countries Vokalia.",
-            // Icon:<HeartHandshake/>,
-            bgColor: "#f5ebeb",
+            icon: <HeartHandshake/>,
+            bgColor: "#A6C9C3",
         },
+
         {
             title: "Amazing Deals",
             description: "Far far away, behind the word mountains, far from the countries Vokalia.",
-            // Icon:<HeartHandshake/>,
-            bgColor: "#f5ebeb",
+            Icon: <HeartHandshake/>,
+            bgColor:"#f5ebeb",
         },
         {
             title: "Amazing Deals",
--- src/pages/landing/components/services.jsx
+++ src/pages/landing/components/services.jsx
@@ -4,7 +4,7 @@ import {HeartHandshake} from "lucide-react"
 
 const Services = () => {
   return (
-    <div className="grid grid-cols-4 gap-x-4">
+    <div className="grid grid-cols-4 py-2 px-2 gap-x-4">
         
         {
           K.SERVICES.map(
